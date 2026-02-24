9948cfa4be7bcef9213aa07bbdaac42c4a4f9db0
eric-engberg <engberg.eric@gmail.com>
2026-02-24 17:55:20 -0600

    Setup the public directory

--- src/main.py
+++ src/main.py
@@ -1,6 +1,25 @@
-from textnode import TextNode, TextType
-def main(): 
-    print(TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev"))
+import os, shutil
+
+def main():
+    setup_public_directory()
+
+def setup_public_directory():
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+        os.mkdir("public")
+
+    copy_dir("static", "public")
+
+def copy_dir(src_dir, dest_dir):
+    for file in os.listdir(src_dir):
+        full_src_path = os.path.join(src_dir, file)
+        full_dest_path = os.path.join(dest_dir, file)
+        if os.path.isfile(full_src_path):
+            print(f"Copying {full_src_path} to {full_dest_path}")
+            shutil.copy(full_src_path, full_dest_path)
+        else:
+            os.mkdir(full_dest_path)
+            copy_dir(full_src_path, full_dest_path)
 
 if __name__ == "__main__":
     main()
